35d83c1e060fccd534a8e69e3c1cd4aa258234bb
mtennekes <mtennekes@gmail.com>
2018-02-03 15:21:56 +0100

    check() ok

--- R/bb.R
+++ R/bb.R
@@ -255,6 +255,9 @@ get_bb <- function(x, cx=NULL, cy=NULL, width=NULL, height=NULL, xlim=NULL, ylim
         b <- structure(c(xlim[1], ylim[1], xlim[2], ylim[2]), names = c("xmin", "ymin", "xmax", "ymax"), class="bbox")
     }
     if (is.null(current.projection)) current.projection <- st_crs(NA)
+
+    attr(b, "crs") <- current.projection
+
     list(b=b,
          cx=cx,
          cy=cy,
